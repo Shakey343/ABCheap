a216148991908fd0b0db30d6df87afdf535813a7
Henrique Langnas <henri.slang@gmail.com>
2022-02-28 17:54:03 +0000

    updated

--- app/models/parameter.rb
+++ app/models/parameter.rb
@@ -1,7 +1,7 @@
 class Parameter < ApplicationRecord
   belongs_to :user_id
+  has_many :options
 
   validates :origin, presence: true
   validates :destination, presence: true
-  validates :earliest_start, :latest_finish, presence: true
 end
