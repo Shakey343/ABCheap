a4742860e8de99c1604981c2842eb43b7ae15d1d
Jake Pople <jakepople@hotmail.co.uk>
2022-03-02 18:03:57 +0000

    form styling updated

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,8 +3,12 @@ import flatpickr from "flatpickr";
 const initFlatpickr = () => {
   flatpickr(".datepicker", {
       enableTime: true,
-      dateFormat: "Y-m-d H:i"
+      dateFormat: "Y-m-d H:i",
   });
+
+  flatpickr(".daterangepicker", {
+    mode: 'range'
+});
 }
 
 export { initFlatpickr };
