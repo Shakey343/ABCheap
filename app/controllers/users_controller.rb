0c5c953522fea352f8249452dcc90713f96b2dd5
Jake Pople <jakepople@hotmail.co.uk>
2022-03-03 16:17:59 +0000

    user login and logout now functioning properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @user_bookings = Booking.where(user_id: current_user)
-    @user_planets = Planet.where(user_id: current_user)
   end
 
   private
