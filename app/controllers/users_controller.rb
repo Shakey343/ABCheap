fdaed2caed58406a7c87c41d560b774f00cae6b7
Jake Pople <jakepople@hotmail.co.uk>
2022-03-03 17:14:02 +0000

    styling added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @user_bookings = Booking.where(user_id: current_user)
+    @favourites = FakeData.where(user_id: current_user)
+    @recents = FakeData.where(user_id: current_user)
+    @upcoming = FakeData.where(user_id: current_user)
   end
 
   private
