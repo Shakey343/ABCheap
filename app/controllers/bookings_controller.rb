4194dea85a72df7c00e72898b4975177ef16806a
Jake Pople <jakepople@hotmail.co.uk>
2022-03-04 14:55:45 +0000

    through to confirmation page now

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,15 @@
 class BookingsController < ApplicationController
-
   before_action :set_booking, only: [:show, :destroy]
 
+  def new
+  end
+
   def create
-    @trip = FakeData.find(booking_params[:fake_data_id])
-    @parameter = Parameter.find(params[:parameter_id])
-    @booking = Booking.new
+    @booking = Booking.new(booking_params)
+    # @parameter = Parameter.find(params[:parameter_id])
+    trip = FakeData.find(params[:booking][:fake_data_id])
     @booking.user = current_user
-    @booking.fake_data = @trip
+    @booking.fake_data = trip
 
     if @booking.save
       redirect_to booking_path(@booking)
@@ -28,6 +30,7 @@ class BookingsController < ApplicationController
   end
 
   private
+
   def booking_params
     params.require(:booking).permit(:fake_data_id)
   end
