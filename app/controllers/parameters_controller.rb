3144de8da6cd8955c8977a6394ec10a2f585f9ac
Alex Taylor <alexhenrytaylor@gmail.com>
2022-03-09 13:52:39 +0000

    railcard and car functionality, needs a bit of styling

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -8,7 +8,29 @@ class ParametersController < ApplicationController
     # FakeData.destroy_all
     @booking = Booking.new
     @parameter = Parameter.find(params[:id])
+    @parameter.update(origin: Geocoder.search(@parameter.origin).first.data["address"]["city"])
+    @parameter.update(destination: Geocoder.search(@parameter.destination).first.data["address"]["city"])
     FakeData.generate_results(@parameter)
+
+    if @parameter.car
+      lat_diff = (Geocoder.search(@parameter.origin).first.data["lat"].to_f - Geocoder.search(@parameter.destination).first.data["lat"].to_f).abs
+      lon_diff = (Geocoder.search(@parameter.origin).first.data["lon"].to_f - Geocoder.search(@parameter.destination).first.data["lon"].to_f).abs
+
+
+      distance = (Math.sqrt((lat_diff * lat_diff) + (lon_diff * lon_diff)) * 73).to_f
+      time = ((distance / 54) * 60).to_i
+      price_per_mile = 0.2
+      cost = distance * price_per_mile
+      FakeData.create!(
+        origin: @parameter.origin,
+        destination: @parameter.destination,
+        cost: cost.round(2),
+        start_time: @parameter.preferred_start + 100,
+        end_time: @parameter.preferred_start + (time * 60),
+        duration: time,
+        mode: "car"
+      )
+    end
 
     if @parameter.earliest_start.nil?
       if @parameter.preferred_start - 14400 < DateTime.now
@@ -24,7 +46,7 @@ class ParametersController < ApplicationController
       if FakeData.all.count != 0 && FakeData.last.mode != "bus"
         latest_finish_date = @parameter.preferred_start + (120 * FakeData.last.duration) + 7200
       elsif FakeData.all.count != 0 && FakeData.last.mode == "bus"
-        latest_finish_date = @parameter.preferred_start + FakeData.last.duration + 7200
+        latest_finish_date = @parameter.preferred_start + (60 * FakeData.last.duration) + 7200
       else
         latest_finish_date = DateTime.now
       end
@@ -32,7 +54,13 @@ class ParametersController < ApplicationController
       latest_finish_date = @parameter.latest_finish
     end
 
-    valid_data = FakeData.where("cost != 0 AND booked != true AND end_time < ? AND start_time > ?", latest_finish_date, earliest_start_date)
+    valid_data = FakeData.where("cost != 0 AND booked != true AND end_time <= ? AND start_time >= ?", latest_finish_date, earliest_start_date)
+
+    if @parameter.railcard
+      valid_data.where("mode = 'train'").each do |result|
+        result.update(cost: (result.cost * 2 / 3).round(2))
+      end
+    end
 
     if valid_data.all.count.zero?
       # @fastest = @cheapest = @recommended = FakeData.create!(
@@ -100,9 +128,29 @@ class ParametersController < ApplicationController
     end
   end
 
+
+  # def driving(parameter)
+  #   lat_diff = (Geocoder.search(parameter.origin).first.data["lat"].to_f - Geocoder.search(parameter.destination).first.data["lat"].to_f).abs
+  #   lon_diff = (Geocoder.search(parameter.origin).first.data["lon"].to_f - Geocoder.search(parameter.destination).first.data["lon"].to_f).abs
+
+  #   distance = (Math.sqrt((lat_diff * lat_diff) + (lon_diff * lon_diff)) * 73).to_i
+  #   time = ((distance / 54) * 60).to_i
+  #   price_per_mile = 0.1
+  #   cost = distance * price_per_mile
+  #   FakeData.create!(
+  #     origin: parameter.origin,
+  #     destination: parameter.destination,
+  #     cost: 5,
+  #     start_time: parameter.preferred_start,
+  #     end_time: parameter.preferred_start + time,
+  #     duration: time,
+  #     mode: "car"
+  #   )
+  # end
+
   private
 
   def set_params
-    params.require(:parameter).permit(:origin, :destination, :preferred_start, :earliest_start, :latest_finish)
+    params.require(:parameter).permit(:origin, :destination, :preferred_start, :earliest_start, :latest_finish, :car, :railcard)
   end
 end
